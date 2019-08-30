13df9fb7be5d31f521a3b2c72b4d2d0d13256a6b
Eduardo Filho <filhoeduardo83@gmail.com>
2019-08-29 22:19:08 -0300

    atualizando para ultima versao do curso

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableFeignClients("com.in28minutes.microservices.currencyconversionservice")
@@ -12,6 +15,13 @@ public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+	
+	
+	//*Método para ativar o Sleuth. Essa ferramenta atribui ID nas requisições, facilitando o rastreio. Todo microservice rastreado deve conter esse método.
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
--- netflix-zuul-api-gateway/src/main/java/com/in28minutes/microservices/netflixzuulapigateway/NetflixZuulApiGatewayApplication.java
+++ netflix-zuul-api-gateway/src/main/java/com/in28minutes/microservices/netflixzuulapigateway/NetflixZuulApiGatewayApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+import io.micrometer.core.instrument.LongTaskTimer.Sample;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -12,6 +16,13 @@ public class NetflixZuulApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NetflixZuulApiGatewayApplication.class, args);
+	}
+	
+
+	//*Método para ativar o Sleuth. Essa ferramenta atribui ID nas requisições, facilitando o rastreio. Todo microservice rastreado deve conter esse método.
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
